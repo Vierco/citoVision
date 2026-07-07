1dff81ed4a7307c91f90b5a6eb924bda5e78a864
SergioA <sergioalvarezb@gmail.com>
2026-07-07 17:37:47 +0200

    fix MainActivity

--- androidApp/src/main/java/dev/lovelace/citovision/MainActivity.kt
+++ androidApp/src/main/java/dev/lovelace/citovision/MainActivity.kt
@@ -1,27 +1,14 @@
 package dev.lovelace.citovision
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(
-            statusBarStyle =
-                SystemBarStyle.light(
-                    Color.TRANSPARENT,
-                    Color.TRANSPARENT,
-                ),
-            navigationBarStyle =
-                SystemBarStyle.light(
-                    Color.TRANSPARENT,
-                    Color.TRANSPARENT,
-                ),
-        )
+        enableEdgeToEdge()
         setContent {
             App()
         }
